4786b44061b5de0311be10aedaf98f175ff6b455
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2024-07-05 07:12:52 +0600

    delete

--- index.js
+++ index.js
@@ -116,6 +116,15 @@ async function run() {
           res.send(result);
 
       })
+    
+      // Delete craft 
+      app.delete('/crafts/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await craftsCollection.deleteOne(query);
+          res.send(result);
+
+      })
       
     
     
